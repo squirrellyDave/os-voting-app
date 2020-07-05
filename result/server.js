c9af0a80bb908289646cb1fe1216a1d37dc75bbe
bl00na <marc.humpert@gmx.net>
2020-07-05 10:42:48 +0200

    corrected pg connection string variable

--- result/server.js
+++ result/server.js
@@ -1,7 +1,12 @@
 var keys = require('./keys');
 var port = keys.appPort;
+var pgUser = keys.pgUser;
+var pgPassword = keys.pgPassword;
+var pgHost = keys.pgHost;
+var pgPort = keys.pgPort
+var pgDatabase = keys.pgDatabase;
 // Syntax: postgres://<username>:<password>@<servername>[:<port>]/<databasename>
-var pgConString = 'postgres://${keys.pgUser}:${keys.pgPassword}@${keys.pgHost}:${keys.pgPort}/${keys.pgDatabase}';
+var pgConString = 'postgres://${pgUser}:${pgPassword}@${pgHost}:${pgPort}/${pgDatabase}';
 console.log("Using connection string: " + pgConString);
 
 var express = require('express'),
