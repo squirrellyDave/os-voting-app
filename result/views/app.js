f2118b13c954fef998d9570e7119eebcb9238968
bl00na <marc.humpert@gmx.net>
2020-10-07 18:15:37 +0200

    binding options to strings

--- result/views/app.js
+++ result/views/app.js
@@ -6,8 +6,8 @@ var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
 
 app.controller('statsCtrl', function($scope) {
-  $scope.option_a = keys.option_a || 'Cats';
-  $scope.option_b = keys.option_b || 'Dogs';
+  $scope.option_a = 'Cats';
+  $scope.option_b = 'Dogs';
   $scope.aPercent = 50;
   $scope.bPercent = 50;
 
